181f9d92a60d236eacb63455029c711f3a5e3a3c
prashant dhote <pdhote106@gmail.com>
2025-11-15 22:51:29 +0530

    react-project is done

---
+++ my-react/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+     <div id="parent"> 
+        <h1 id='child1'>prashnt</h1>
+        <h1 id='child2'>nitin</h1>
+     </div>
+     <div id="chacha"> 
+        <h1 id='child11'>pranay</h1>
+        <h1 id='child22'>mohit</h1>
+     </div>
+
+    </>
+  )
+}
+
+export default App
